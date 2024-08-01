42bdad98e721ece6baec8f10cce54f1fc1a9e9f5
HudsonAdjetey <adjetey545@gmail.com>
2024-08-01 03:24:20 -0700

    page admin started

--- management/app/admin/page.jsx
+++ management/app/admin/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Images } from "@/components/constants/img";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href={"/"} className="cursor-pointer">
+          <Image
+            src={Images.logo}
+            width={32}
+            height={162}
+            className="h-8 w-fit"
+          />
+        </Link>
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
+    </div>
+  );
+};
 
-export default page
+export default page;
