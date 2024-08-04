3e4f8dbcd665f5113a7b5d97127930f58ec2bae9
HudsonAdjetey <adjetey545@gmail.com>
2024-08-04 13:23:04 -0700

    start card stated

--- management/components/StatCard.jsx
+++ management/components/StatCard.jsx
@@ -1,7 +1,8 @@
 import clsx from "clsx";
+import Image from "next/image";
 import React from "react";
 
-const StatCard = ({ type }) => {
+const StatCard = ({ type, icon, count, label }) => {
   return (
     <div
       className={clsx("stat-card", {
@@ -11,7 +12,17 @@ const StatCard = ({ type }) => {
         "bg-faculties": type === "faculties",
       })}
     >
-      StatCard
+      <div className="flex items-center gap-4">
+        <Image
+          src={icon}
+          height={32}
+          width={32}
+          alt={`${type} card`}
+          className="size-8 w-fit"
+        />
+        <h2 className="text-32-bold text-white">{count}</h2>
+      </div>
+      <p className="text-14-regular">{label}</p>
     </div>
   );
 };
